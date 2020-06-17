2d3179e66eddbec6cb9ca0f6996b1e90705bad14
Mattes Sarcander <info@fortschritt.pro>
2020-06-17 09:56:11 +0200

    Django 2.2 compatibility fixes

--- spaces_dashboard/urls.py
+++ spaces_dashboard/urls.py
@@ -4,6 +4,6 @@ from spaces.urls import space_patterns
 from . import views
 
 app_name = 'spaces_dashboard'
-urlpatterns = space_patterns(
+urlpatterns = (
     url(r'^$', views.dashboard, name='dashboard'),
 )
